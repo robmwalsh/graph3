789489871ad5da705b581d2015946f313886e2f2
Rob Walsh <30517573+robmwalsh@users.noreply.github.com>
2022-01-04 12:22:43 +1100

    more

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "graph"
+
+version := "0.1"
+
+scalaVersion := "3.1.0"
--- src/main/scala/graph/Schema.scala
+++ src/main/scala/graph/Schema.scala
@@ -1,9 +1,14 @@
 package graph
 
+import scala.deriving.Mirror
+
 import java.lang.constant.Constable
 
+type #>[Node, Key <: String & Constable] =
+  NodeType[Node, Key]
+
 trait GraphType
-object GraphType
+object GraphType {}
 trait NodeType[Node, Key <: String & Constable] extends GraphType
 trait EdgeType[In <: NodeType[?, ?], Edge, Out <: NodeType[?, ?]]
     extends GraphType
@@ -17,25 +22,28 @@ trait Schema[N <: NodeType[?, ?], E <: EdgeType[? <: N, ?, ? <: N]] {
   def addNode[A <: NodeType[?, ?]]: Schema[N | A, E] = new Schema[N | A, E] {}
   def addEdge[In <: N, Edge, Out <: N]: Schema[N, E | EdgeType[In, Edge, Out]] =
     new Schema[N, E | EdgeType[In, Edge, Out]] {}
+  def addSchema[N0 <: NodeType[?, ?], E0 <: EdgeType[? <: N0, ?, ? <: N0]](
+      that: Schema[N0, E0]
+  ): Schema[N | N0, E | E0] = new Schema[N | N0, E | E0] {}
 }
 
 object Schema extends App {
   val empty = new Schema[Nothing, Nothing] {}
-
+  type PersonNode = Person #> "name"
+  type AddressNode = Address #> "number"
+  type LivesAtEdge = EdgeType[PersonNode, LivesAt.type, AddressNode]
+  type KnowsEdge = EdgeType[PersonNode, Knows.type, PersonNode]
   val test = new Schema[
-    NodeType[Person, "name"] | NodeType[Address, "number"],
-    EdgeType[
-      NodeType[Person, "name"],
-      LivesAt.type,
-      NodeType[Address, "number"]
-    ]
+    PersonNode | AddressNode,
+    LivesAtEdge | KnowsEdge
   ] {}
 
-  val a = empty.addNode[NodeType[Person, "name"]]
-  val b = a.addNode[NodeType[Address, "number"]]
+  val social =
+    empty.addNode[Person #> "name"].addEdge[PersonNode, Knows.type, PersonNode]
+  val delivery = empty
+    .addNode[Person #> "name"]
+    .addNode[AddressNode]
+    .addEdge[PersonNode, LivesAt.type, AddressNode]
+  val socialDelivery = social.addSchema(delivery)
 
-  val c = b.addEdge[NodeType[Person, "name"], LivesAt.type, NodeType[
-    Address,
-    "number"
-  ]]
 }
