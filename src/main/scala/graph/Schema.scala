fe21e7df0e4bb46cea66c3f414e576676e485a6c
Rob Walsh <30517573+robmwalsh@users.noreply.github.com>
2022-01-10 13:49:14 +1100

    more

--- src/main/scala/graph/Schema.scala
+++ src/main/scala/graph/Schema.scala
@@ -1,27 +1,12 @@
 package graph
 
+import graph.Schema.Key.Self
+import graph.Schema.Node.keyFnImpl
+import graph.Schema.{Edge, Node}
+
 import scala.deriving.Mirror
-
 import java.lang.constant.Constable
-import Key._
-import GraphType._
-
-sealed trait Key[T, K] {
-  type Key
-}
-object Key {
-  sealed trait #>[T, K <: String & Constable] extends Key[T, K]
-  // key is tuple of node keys
-  sealed trait Self[T <: Singleton] extends Key[T, T]
-}
-
-sealed trait GraphType {
-
-}
-object GraphType {
-  trait Node[K <: #>[?, ?]] extends GraphType {
-  trait Edge[From <: Node[?], K <: Key[?, ?], To <: Node[?]] extends GraphType
-}
+import scala.quoted.{Expr, Quotes, Type}
 
 case class Person(name: String, age: Int)
 case object LivesAt
@@ -39,12 +24,49 @@ trait Schema[N <: Node[?], E <: Edge[? <: N, ?, ? <: N]] {
   ): Schema[N | N0, E | E0] = new Schema[N | N0, E | E0] {}
 }
 
-object Schema extends App {
+object Schema {
+  import Key._
+  sealed trait GraphObject {
+
+    inline def keyFn[T, K](inline key: Key[T, K]): T => Any
+  }
+  trait Node[K <: Key[?, ?]] extends GraphObject {
+    override transparent inline def keyFn[T, K](
+        inline key: Key[T, K]
+    ): T => Any = ${
+      Node.keyFnImpl[T, K]('{ key })
+    }
+
+  }
+  object Node {
+
+    def keyFnImpl[T: Type, K: Type](key: Expr[Key[T, K]])(using
+        Quotes
+    ): Expr[T => Any] = '{ _.toString }
+  }
+  trait Edge[From <: Node[?], K <: Key[?, ?], To <: Node[?]] extends GraphObject
+  sealed trait Key[T, K] {
+    type Out
+  }
+  object Key {
+    sealed trait #>[T, K <: String & Constable] extends Key[T, K]
+    object #> {
+      type Aux[T, K <: String & Constable, O] = #>[T, K] { type Out = O }
+    }
+    sealed trait Self[T <: Singleton] extends Key[T, T] {
+      override type Out = T
+    }
+  }
+
+}
+
+object Test extends App {
+  import Schema.Key._
   val empty = new Schema[Nothing, Nothing] {}
   type PersonNode = Node[Person #> "name"]
   type AddressNode = Node[Address #> "number"]
-  type LivesAtEdge = Edge[PersonNode, Label["LivesAt"], AddressNode]
-  type KnowsEdge = Edge[PersonNode, Label[Knows.type], PersonNode]
+  type LivesAtEdge = Edge[PersonNode, Self["LivesAt"], AddressNode]
+  type KnowsEdge = Edge[PersonNode, Self[Knows.type], PersonNode]
   val test = new Schema[
     PersonNode | AddressNode,
     LivesAtEdge | KnowsEdge
@@ -57,5 +79,4 @@ object Schema extends App {
     .addNode[AddressNode]
     .addEdge[LivesAtEdge]
   val socialDelivery = social.addSchema(delivery)
-
 }
