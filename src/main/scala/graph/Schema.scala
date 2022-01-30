1ea7086ab6ee2f6b6453056fde3e4c501a4f6c34
Rob Walsh <30517573+robmwalsh@users.noreply.github.com>
2022-01-30 20:13:43 +1100

    more

--- src/main/scala/graph/Schema.scala
+++ src/main/scala/graph/Schema.scala
@@ -1,7 +1,7 @@
 package graph
 
 import graph.Schema.Key.Self
-import graph.Schema.Node.keyFnImpl
+
 import graph.Schema.{Edge, Node}
 import scala.compiletime.*
 import scala.quoted.*
@@ -10,105 +10,98 @@ import scala.deriving.Mirror
 import java.lang.constant.Constable
 import scala.Tuple.Union
 import scala.quoted.{Expr, Quotes, Type}
+import java.lang.constant.Constable
 
 case class Person(name: String, age: Int)
 case object LivesAt
 case object Knows
 case class Address(number: Int, street: String)
 
-trait Schema[N <: Node[?], E <: Edge[? <: N, ?, ? <: N]] {
-  def addNode[N0 <: Node[?]]: Schema[N | N0, E] = new Schema[N | N0, E] {}
+trait Schema[N <: Node[?, ?], E <: Edge[? <: N, ?, ?, ? <: N]] {
+  def addNode[N0 <: Node[?,?]]: Schema[N | N0, E] = new Schema[N | N0, E] {}
   def addEdge[
-      E0 <: Edge[? <: N, ?, ? <: N]
+      E0 <: Edge[? <: N, ?, ?, ? <: N]
   ]: Schema[N, E | E0] =
     new Schema[N, E | E0] {}
-  def addSchema[N0 <: Node[?], E0 <: Edge[? <: N0, ?, ? <: N0]](
+  def addSchema[N0 <: Node[?,?], E0 <: Edge[? <: N0, ? ,?, ? <: N0]](
       that: Schema[N0, E0]
   ): Schema[N | N0, E | E0] = new Schema[N | N0, E | E0] {}
 }
 
 object Schema {
   import Key._
-  sealed trait GraphObject[K <: Key.Aux[?, ?, ?]] {
-    val key: K
-
+  sealed trait GraphObject[T, O] {
+    type K <: String & Constable
+    private[GraphObject] val key0: Key.Aux[T, K, O]
+    
     // transparent inline def keyFn[T, K](inline key: Key[T, K]): T => Any
   }
-  sealed trait Node[K <: Key.Aux[?, ?, ?]] extends GraphObject[K] {}
+  sealed trait Node[T,O] extends GraphObject[T, O] {
+    
+  }
   object Node {
-    transparent inline def apply[K <: Key[?, ?]]: Node[Key.Aux[Any, Any, Any]] =
+    transparent inline def apply[K <: Key[?, ?]]: Any =
       ${
         applyFnImpl[K]
       }
     private def applyFnImpl[K <: Key[?, ?]: Type](using
         Quotes
-    ): Expr[Node[Key.Aux[Any, Any, Any]]] = {
+    ): Expr[Any] = {
       import quotes.reflect._
-      def loop[L: Type, T: Type, K: Type]: TypeRepr =
+
+      def loop[L: Type, T: Type, K: Type]: TypeRepr = {
         Type.of[L] match
           case '[lHead *: lTail] =>
             Type.of[T] match
-              case '[tHead *: tTail] => {
-                if (Type.of[lHead] == Type.of[K]) TypeRepr.of[tHead]
-                else loop[lTail, tTail, T]
-              }
+              case '[tHead *: tTail] =>
+                if (TypeRepr.of[lHead] == TypeRepr.of[K]) TypeRepr.of[tHead]
+                else loop[lTail, tTail, K]
+          case '[EmptyTuple] => ???
+      }
 
       Type.of[K] match {
-        case '[Key[t, k]] => {
-          Expr.summon[Mirror.ProductOf[k]].get match {
+        case '[Key[t, k]] =>
+          Expr.summon[Mirror.ProductOf[t]].get match {
             case '{
-                  $m: Mirror.ProductOf[k] {
+                  $m: Mirror.ProductOf[t] {
                     type MirroredElemLabels = labels;
                     type MirroredElemTypes = types
                   }
                 } =>
-              loop[labels, types, k]
-
-            case _ =>
-              throw new IllegalStateException(
-                "labels and types must be the same length"
-              )
-
+              loop[labels, types, k].asType match {
+                case '[r] =>
+                  '{
+                    new Node[t, r] {
+                      val key0 = new Key[t,k]{type T_ = t; type K_ = k; type O_ = r }
+                    }
+                  }
+              }
           }
-        }
-        case _ => throw new Exception("uh oh")
       }
     }
   }
-  trait Edge[From <: Node[?], K <: Key.Aux[?, ?, ?], To <: Node[?]]
-      extends GraphObject[K]
-
+  sealed trait Edge[From <: Node[_, _], T, O, To <: Node[_,_]]
+      extends GraphObject[T,O]
+  //
   sealed trait Key[T, K] {
-    type Out
-    val keyFn: T => Out
+    type O_
+    val keyFn: T => O_
+    def apply(t: T): O_ = keyFn(t)
   }
 
   object Key {
-    type Aux[T, K <: String & Constable, O] = Key[T, K] { type Out = O }
+    def apply[T, K, O](keyFn0: T => O): Aux[T, K, O] = new Key[T, K] {
+      self =>
+      override type T_ = T
+      override type K_ = K
+      override type O_ = O
+      override val keyFn: T => O_ = keyFn0
+    }
+
+    type Aux[T, K, O] = Key[T, K] { type T_ = T; type K_ = K; type O_ = O }
 
     sealed trait #>[T, K <: String & Constable] extends Key[T, K]
     sealed trait Self[T <: Singleton] extends Key[T, T]
   }
 
-}
-
-object Test extends App {
-  import Schema.Key._
-  val empty = new Schema[Nothing, Nothing] {}
-  type PersonNode = Node[Person #> "name"]
-  type AddressNode = Node[Address #> "number"]
-  type LivesAtEdge = Edge[PersonNode, Self["LivesAt"], AddressNode]
-  type KnowsEdge = Edge[PersonNode, Self[Knows.type], PersonNode]
-  val test = new Schema[
-    PersonNode | AddressNode,
-    LivesAtEdge | KnowsEdge
-  ] {}
-
-  val social =
-    empty.addNode[PersonNode].addEdge[KnowsEdge]
-  val delivery = empty
-    .addNode[PersonNode]
-    .addNode[AddressNode]
-    .addEdge[LivesAtEdge]
-  val socialDelivery = social.addSchema(delivery)
 }
--- src/main/scala/graph/Test.scala
+++ src/main/scala/graph/Test.scala
@@ -1,2 +1,25 @@
 package graph
-object Test {}
+
+import graph.Schema.{Edge, Node}
+
+object Test extends App {
+  import Schema.Key.*
+  val empty = new Schema[Nothing, Nothing] {}
+  val PersonNode = Node[Person #> "name"]
+  val AddressNode = Node[Address #> "number"]
+  
+  ///val LivesAtEdge = Edge[PersonNode, Self["LivesAt"], AddressNode]
+  /*type KnowsEdge = Edge[PersonNode, Self[Knows.type], PersonNode]
+  val test = new Schema[
+    PersonNode | AddressNode,
+    LivesAtEdge | KnowsEdge
+  ] {}
+
+  val social =
+    empty.addNode[PersonNode].addEdge[KnowsEdge]
+  val delivery = empty
+    .addNode[PersonNode]
+    .addNode[AddressNode]
+    .addEdge[LivesAtEdge]
+  val socialDelivery = social.addSchema(delivery)*/
+}
